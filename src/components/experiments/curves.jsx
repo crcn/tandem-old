ab981f40d8f1b23851569271f2b21f19cfa74320
Craig Condon <craig.j.condon@gmail.com>
2015-11-04 10:39:43 -0800

    update components

---
+++ src/components/experiments/curves.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+var CurvesComponent = React.createClass({
+  render: function() {
+    
+    var data = [
+      {
+        name: 'test',
+        type: 'box',
+        x: 100,
+        y: 100,
+        width: 100,
+        height: 100,
+        outputs: [
+          { name: 'test2' }
+        ]
+      },
+      {
+        name: 'test2',
+        type: 'box',
+        x: 300,
+        y: 300,
+        width: 100,
+        height: 100
+      }
+    ]
+  };
+
+});
+
+export default CurvesComponent;
