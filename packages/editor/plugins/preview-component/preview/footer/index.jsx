cbd285280d1e1f9c426220915d1fabf0ad1969cf
Craig Condon <craig.j.condon@gmail.com>
2015-12-09 21:58:10 -0800

    fix zoom & add more info in footer

--- packages/editor/plugins/preview-component/preview/footer/index.jsx
+++ packages/editor/plugins/preview-component/preview/footer/index.jsx
@@ -5,9 +5,12 @@ class FooterComponent extends React.Component {
   render() {
 
     // TODO - add more project information here such as file name
+    // TODO - ability to edit canvas width & height in the footer
     var app = this.props.app;
     return <div className='m-preview-footer'>
       { Math.round(app.preview.zoom * 100) }%
+      &nbsp;
+      { app.preview.canvasWidth }W x { app.preview.canvasHeight }H
     </div>;
   }
 }
