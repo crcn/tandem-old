4d68fcc66f4051eb4ffee471cd3f612421fc81c5
Craig Condon <craig.j.condon@gmail.com>
2015-12-13 13:36:08 -0800

    start on new panes

---
+++ packages/editor/plugins/basic-dom-entities/components/entity-panes/transform/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class TransformPaneComponent extends React.Component {
+  render() {
+    return <div className='m-transform-pane'>
+
+    </div>;
+  }
+}
+export default TransformPaneComponent;
---
+++ packages/editor/plugins/basic-dom-entities/components/entity-panes/typography/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class TypographyPaneComponent extends React.Component {
+  render() {
+    return <div className='m-typography-pane'>
+
+    </div>;
+  }
+}
+export default TypographyPaneComponent;
