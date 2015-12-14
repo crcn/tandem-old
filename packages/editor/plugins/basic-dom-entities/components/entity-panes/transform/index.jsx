1cff5d6755cd398a0c52d79251302270ffe777f8
Craig Condon <craig.j.condon@gmail.com>
2015-12-13 16:30:26 -0800

    start on forms

--- packages/editor/plugins/basic-dom-entities/components/entity-panes/transform/index.jsx
+++ packages/editor/plugins/basic-dom-entities/components/entity-panes/transform/index.jsx
@@ -1,8 +1,47 @@
+import './index.scss';
 import React from 'react';
 
 class TransformPaneComponent extends React.Component {
   render() {
     return <div className='m-transform-pane'>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-3'>
+            <label>x</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+            <label>y</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+            <label>w</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+            <label>h</label>
+            <input type='text'></input>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-sm-3'>
+            <label>rx</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+            <label>ry</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+            <label>rz</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+          </div>
+        </div>
+      </div>
 
     </div>;
   }
--- packages/editor/plugins/basic-dom-entities/components/entity-panes/typography/index.jsx
+++ packages/editor/plugins/basic-dom-entities/components/entity-panes/typography/index.jsx
@@ -1,9 +1,39 @@
+import './index.scss';
+
 import React from 'react';
 
 class TypographyPaneComponent extends React.Component {
   render() {
     return <div className='m-typography-pane'>
-      type
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <label>Family</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-6'>
+            <label>Style</label>
+            <input type='text'></input>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-sm-3'>
+            <label>Size</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-3'>
+            <label>Color</label>
+            <input type='text'></input>
+          </div>
+          <div className='col-sm-6'>
+            <label>Align</label>
+            <input type='text'></input>
+          </div>
+        </div>
+      </div>
+
     </div>;
   }
 }
