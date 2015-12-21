9ab61114423afbbeed43fe6c42155097b2682ba7
Craig Condon <craig.j.condon@gmail.com>
2015-12-21 11:05:12 -0800

    update padding

--- packages/common/components/text-editor/components/line.jsx
+++ packages/common/components/text-editor/components/line.jsx
@@ -25,14 +25,14 @@ class LineComponent extends React.Component {
     return <div ref='line' className='m-text-editor--line' onMouseDown={this.onClick.bind(this)}
     onDoubleClick={this.onDoubleClick.bind(this)}>
       {
-        line.tokens.map((token, i) => {
+        line.tokens.length ? line.tokens.map((token, i) => {
           return <TokenComponent
             editor={this.props.editor}
             tokenComponentFactory={this.props.tokenComponentFactory}
             token={token}
             line={line}
             key={token.toString() + ',' + i} />;
-        })
+        }) : <span>&nbsp;</span>
       }
     </div>;
   }
