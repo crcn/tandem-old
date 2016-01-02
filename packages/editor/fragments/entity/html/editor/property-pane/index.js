7e98ecfdf5d3c3bc78ba53cf38894a8f43c63e4d
Craig Condon <craig.j.condon@gmail.com>
2016-01-01 17:45:31 -0800

    combine fragments

--- packages/editor/fragments/entity/html/editor/property-pane/index.js
+++ packages/editor/fragments/entity/html/editor/property-pane/index.js
@@ -1,28 +1,134 @@
 import {
+  ComponentFragment,
   EntityPaneComponentFragment
 } from 'editor/fragment/types';
 
-import StylePaneComponent from './components/style-pane';
 import inflection from 'inflection';
+import StylePaneComponent from './components/style-pane';
+import TextInputComponent from 'common/components/inputs/text';
+import UnitInputComponent from 'common/components/inputs/unit';
+import FontInputComponent from './components/style-inputs/font';
+import ColorPickerComponent from 'common/components/inputs/color-picker';
+import TextAlignInputComponent from './components/style-inputs/text-align';
+import BackgroundInputComponent from './components/style-inputs/background';
 
-import { create as createStyleInputFragments } from './fragments/style-inputs';
 import { LAYOUT, TRANSFORM, APPEARANCE, TYPOGRAPHY } from './categories';
 
 export function create({ app }) {
 
   return [
     ...createStyleInputFragments({ app }),
-    ...[LAYOUT, TRANSFORM, TYPOGRAPHY, APPEARANCE].map(function(category) {
-      return EntityPaneComponentFragment.create({
-        id             : category + 'PaneComponent',
-
-        // TODO - use uppercase lib
-        label          : inflection.titleize(category),
-        styleCategory  : category,
-        paneType       : 'entity',
-        entityType     : 'component',
-        componentClass : StylePaneComponent
-      })
-    })
+    ...createEditorPaneFragments({ app })
   ];
+}
+
+
+function createStyleInputFragments({ app }) {
+  var fragments = [];
+
+  var inf = {
+    [LAYOUT]: [
+      [ 'display'  , TextInputComponent ],
+      [ 'position' , TextInputComponent ],
+
+      // drop menu of options ere
+      [ 'float'        , TextInputComponent ],
+
+      [ 'margin'       , UnitInputComponent ],
+      [ 'marginLeft'   , UnitInputComponent ],
+      [ 'marginTop'    , UnitInputComponent ],
+      [ 'marginRight'  , UnitInputComponent ],
+      [ 'marginBottom' , UnitInputComponent ],
+
+      [ 'padding'       , UnitInputComponent ],
+      [ 'paddingLeft'   , UnitInputComponent ],
+      [ 'paddingTop'    , UnitInputComponent ],
+      [ 'paddingRight'  , UnitInputComponent ],
+      [ 'paddingBottom' , UnitInputComponent ]
+    ],
+    [TRANSFORM]: [
+
+      // need to display warning if position is static
+      [ 'left'   , UnitInputComponent ],
+      [ 'top'    , UnitInputComponent ],
+      [ 'width'  , UnitInputComponent ],
+      [ 'height' , UnitInputComponent ],
+
+      // css3
+      [ 'transform', TextInputComponent ]
+    ],
+    [TYPOGRAPHY]: [
+      [ 'fontFamily'     , FontInputComponent      ],
+      [ 'fontSize'       , UnitInputComponent      ],
+
+      // http://www.w3schools.com/cssref/pr_font_font-style.asp
+      [ 'fontStyle'      , TextInputComponent      ],
+      [ 'fontWeight'     , TextInputComponent      ],
+
+      // http://www.w3schools.com/cssref/pr_text_text-decoration.asp
+      [ 'textDecoration' , TextInputComponent      ],
+      [ 'textAlign'      , TextAlignInputComponent ],
+      [ 'wordWrap'       , TextInputComponent      ],
+
+      // drop menu here
+      [ 'textOverflow'  , TextInputComponent   ],
+      [ 'color'      , ColorPickerComponent    ]
+    ],
+    [APPEARANCE]: [
+
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/background
+      [ 'background'   , BackgroundInputComponent ],
+      [ 'borderRadius' , UnitInputComponent       ],
+
+      // TODO - box shadow component here
+      [ 'boxShadow'    , TextInputComponent       ],
+
+      // TODO - slider component from 0 - 100
+      [ 'opacity'      , UnitInputComponent       ],
+
+      [ 'border'       , TextInputComponent       ],
+      [ 'borderLeft'   , TextInputComponent       ],
+      [ 'borderTop'    , TextInputComponent       ],
+      [ 'borderRight'  , TextInputComponent       ],
+      [ 'borderBottom' , TextInputComponent       ],
+
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/mix-blend-mode
+      [ 'mixBlendMode' , TextInputComponent       ]
+    ]
+  }
+
+  for (var category in inf) {
+
+    for (var [styleName, componentClass] of inf[category]) {
+      fragments.push(createComponentStyleFragment(styleName, category, componentClass));
+    }
+  }
+
+  return fragments;
+}
+
+
+function createComponentStyleFragment(name, category, componentClass) {
+  return ComponentFragment.create({
+    id             : name + 'StyleInputComponent',
+    componentType  : 'styleInput',
+    componentClass : componentClass,
+    styleName      : name,
+    styleCategory  : category
+  })
+}
+
+function createEditorPaneFragments({ app }) {
+  return [LAYOUT, TRANSFORM, TYPOGRAPHY, APPEARANCE].map(function(category) {
+    return EntityPaneComponentFragment.create({
+      id             : category + 'PaneComponent',
+
+      // TODO - use uppercase lib
+      label          : inflection.titleize(category),
+      styleCategory  : category,
+      paneType       : 'entity',
+      entityType     : 'component',
+      componentClass : StylePaneComponent
+    })
+  });
 }
