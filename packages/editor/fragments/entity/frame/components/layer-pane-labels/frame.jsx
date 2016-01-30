cfcb6839163eed73b5ffeeb34d68203dedcf8096
Craig Condon <craig.j.condon@gmail.com>
2016-01-30 14:42:02 -0800

    style frame layer

--- packages/editor/fragments/entity/frame/components/layer-pane-labels/frame.jsx
+++ packages/editor/fragments/entity/frame/components/layer-pane-labels/frame.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 class FrameLabelComponent extends React.Component {
   render() {
-    return <div className='m-frame-label'>{ this.props.entity.attributes.label || 'Frame'}</div>;
+    return <div className='m-frame-label'>
+        { this.props.entity.label || 'Frame'}
+    </div>;
   }
 }
 
