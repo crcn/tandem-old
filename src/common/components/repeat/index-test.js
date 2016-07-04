3b1fe52110a62a3280d5429bbca8eec02331b211
Craig Condon <craig.j.condon@gmail.com>
2016-07-04 17:28:06 +0200

    add one more test

--- src/common/components/repeat/index-test.js
+++ src/common/components/repeat/index-test.js
@@ -12,4 +12,16 @@ describe(__filename + '#', function() {
     view.update();
     expect(view.section.toString()).to.be('023');
   });
+
+  it('can be multi-nested', function() {
+    var tpl = freeze(<Repeat each={a=>a} as='b'>
+      <Repeat each={c=>c.b} as='c'>
+        {c=>c.c}
+      </Repeat>
+    </Repeat>);
+
+    var view = tpl.createView([[1, 2], ['a', 'b'], ['111', '222']]);
+
+    expect(view.section.toString()).to.be('12ab111222');
+  })
 });
