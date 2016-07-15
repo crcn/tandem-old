ae0aee2131c383118a1598f928c3a9f1f07ef430
Craig Condon <craig.j.condon@gmail.com>
2016-07-15 09:52:30 +0200

    require expression for entities

--- src/common/entities/entity.js
+++ src/common/entities/entity.js
@@ -1,5 +1,9 @@
 import Element from 'common/nodes/element';
+import assertPropertyExists from 'common/utils/assert/property-exists';
 
 export default class Entity extends Element {
-
+  constructor(properties) {
+    super(properties);
+    assertPropertyExists(this, 'expression');
+  }
 }
---
+++ src/editor/models/sfn-file.js
@@ -0,0 +1,11 @@
+import { FactoryFragment } from 'common/fragments';
+import BaseModel from 'common/models/base';
+
+export default class SfnFile extends BaseModel {
+
+}
+
+export default FactoryFragment.create({
+  ns: 'models/sfn-file',
+  factory: SfnFile
+});
