eeafbae43bd6c9546712a476a94b72813e105fe6
Craig Condon <craig.j.condon@gmail.com>
2016-07-18 10:44:06 +0200

    add utils

---
+++ scripts/utils/exec.js
@@ -0,0 +1,10 @@
+var spawn = require('child_process').spawn;
+
+module.exports = function(command, args, options) {
+  return new Promise(function(resolve, reject) {
+    var proc = spawn(command, args, options);
+    proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stderr);
+    proc.on('close', resolve);
+  })
+}
---
+++ sfconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'bundles': [
+    __dirname + '/packages/saffron-front-end',
+    __dirname + '/packages/saffron-html-bundle',
+    __dirname + '/packages/saffron-sass-bundle',
+    __dirname + '/packages/saffron-editor-bundle' 
+  ]
+}
