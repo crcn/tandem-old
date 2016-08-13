52cbc3cc57fd871012767d085fbe2abe57137372
Craig Condon <craig.j.condon@gmail.com>
2016-08-13 21:25:06 +0200

    reset all styles

--- src/sf-front-end/components/root/center/editor/canvas/preview/index.tsx
+++ src/sf-front-end/components/root/center/editor/canvas/preview/index.tsx
@@ -5,7 +5,7 @@ import RegisteredComponent from "sf-front-end/components/registered";
 
 export default class PreviewComponent extends React.Component<{ zoom: number, entity: any }, any> {
   render() {
-    return (<div className="m-editor-stage-preview">
+    return (<div className="m-editor-stage-preview reset-all">
         <RegisteredComponent {...this.props} ns="components/preview" />
     </div>);
   }
