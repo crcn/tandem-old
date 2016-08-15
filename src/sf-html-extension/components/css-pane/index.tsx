4346e41481e52b9b85c54a6e45340db0e6dd9d00
Craig Condon <craig.j.condon@gmail.com>
2016-08-15 12:43:49 +0200

    clean up ui

--- src/sf-html-extension/components/css-pane/index.tsx
+++ src/sf-html-extension/components/css-pane/index.tsx
@@ -1,3 +1,4 @@
+import "./index.scss";
 import * as React from "react";
 import { Workspace } from "sf-front-end/models";
 import PaneComponent from "sf-front-end/components/pane";
@@ -6,6 +7,19 @@ import { parse as parseCSS } from "sf-html-extension/parsers/css";
 import { PaneComponentFactoryDependency } from "sf-front-end/dependencies";
 import { CSSExpression, CSSStyleExpression, CSSStyleDeclarationExpression } from "sf-html-extension/parsers/css/expressions";
 
+class StyleDeclarationComponent extends React.Component<{ declaration: any }, any> {
+  render() {
+    const declaration = this.props.declaration;
+    return <div className="m-css-style-pane--declaration row">
+      <div className="m-css-style-pane--declaration--key">
+        { declaration.key }
+      </div>
+      <div className="m-css-style-pane--declaration--value">
+        { String(declaration.value) }
+      </div>
+    </div>;
+  }
+}
 class StylePaneComponent extends React.Component<any, any> {
   render() {
     const entity: HTMLElementEntity = this.props.entity;
@@ -14,7 +28,7 @@ class StylePaneComponent extends React.Component<any, any> {
     return <div className="m-css-style-pane">
       {
         styleExpression.declarations.map((declaration) => (
-          <div>{ declaration.key }: { String(declaration.value) }</div>
+          <StyleDeclarationComponent {...this.props} declaration={declaration} key={declaration.key} />
         ))
       }
     </div>;
@@ -28,7 +42,7 @@ export class CSSPaneComponent extends React.Component<{ workspace: Workspace }, 
 
     if (!selection.length) return null;
 
-    return <div className="m-css-pane">
+    return <div className="m-css-pane m-pane-container--content">
       <PaneComponent title="style">
         <StylePaneComponent entity={selection[0]} />
       </PaneComponent>
