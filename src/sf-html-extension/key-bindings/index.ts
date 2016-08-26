6791ebeb478e6f901cb50d2217dd8c8b82868404
Craig Condon <craig.j.condon@gmail.com>
2016-08-26 19:02:15 +0200

    start on group key command

--- src/sf-html-extension/key-bindings/index.ts
+++ src/sf-html-extension/key-bindings/index.ts
@@ -2,11 +2,13 @@ import { inject } from "sf-core/decorators";
 import { Action } from "sf-core/actions";
 import { IEditor } from "sf-front-end/models/base";
 import { InsertTool } from "sf-front-end/models/insert-tool";
-import { BaseCommand } from "sf-core/commands";
 import { SetToolAction } from "sf-front-end/actions";
-import { parse as parseHTML } from "sf-html-extension/parsers/html";
 import { TEXT_TOOL_KEY_CODE } from "sf-html-extension/constants";
+import { FrontEndApplication } from "sf-front-end/application";
+import { HTMLEntityDisplaySelection } from "sf-html-extension/models";
+import { BaseCommand, BaseApplicationCommand } from "sf-core/commands";
 import { dependency as pointerToolDependency } from "sf-front-end/models/pointer-tool";
+import { parse as parseHTML, HTMLElementExpression } from "sf-html-extension/parsers/html";
 import { TextTool, dependency as textToolDependency } from "sf-html-extension/models/text-tool";
 import { ClassFactoryDependency, DEPENDENCIES_NS, Dependencies } from "sf-core/dependencies";
 import { EditorToolFactoryDependency, GlobalKeyBindingDependency } from "sf-front-end/dependencies";
@@ -46,6 +48,18 @@ export const dependencies = [
       this.bus.execute(new SetToolAction(this.dependencies.query<EditorToolFactoryDependency>(textToolDependency.ns)));
     }
   })
+  // new GlobalKeyBindingDependency("meta+g", class GroupElementsCommand extends BaseApplicationCommand<FrontEndApplication> {
+  //   execute(action: Action) {
+  //     const selection = <HTMLEntityDisplaySelection><any>this.app.workspace.selection;
+  //     const bounds = selection.display.bounds;
+  //     const element: HTMLElementExpression = <HTMLElementExpression>parseHTML(`<div style="position:absolute;left:${bounds.left}px;top${bounds.top}px;" />`).childNodes[0];
+  //     this.app.workspace.selection.forEach((entity) =>  {
+  //       element.appendChildNodes(entity.source);
+  //       entity.parentNode.removeChild(entity);
+  //     });
+  //     this.app.workspace.file.save();
+  //   }
+  // })
 ];
 
 const insertElementKeyBindings = {
