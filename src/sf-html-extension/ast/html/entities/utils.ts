13984d1ebc634c32e6f6f5aa3f3c17c7fdd0f8ae
Craig Condon <craig.j.condon@gmail.com>
2016-08-30 17:24:42 +0200

    do not trim whitespace

--- src/sf-html-extension/ast/html/entities/utils.ts
+++ src/sf-html-extension/ast/html/entities/utils.ts
@@ -4,6 +4,7 @@ import { IHTMLEntity } from "./base";
 export function disposeEntity(entity: IHTMLEntity) {
   if (entity.parent) {
     const childNodes = (<IHTMLEntity><any>entity.parent).source.children;
+    console.log(entity.source);
     childNodes.splice(childNodes.indexOf(<any>entity.source), 1);
     entity.parent.removeChild(entity);
   }
