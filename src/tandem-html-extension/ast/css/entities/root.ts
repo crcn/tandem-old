84d8bb20959285721f80afee54c29127b59f054c
Craig Condon <craig.j.condon@gmail.com>
2016-09-08 22:11:32 +0200

    detect when css changes in tree

--- src/tandem-html-extension/ast/css/entities/root.ts
+++ src/tandem-html-extension/ast/css/entities/root.ts
@@ -1,14 +1,11 @@
-import { inject } from "tandem-common/decorators";
+import { WrapBus } from "mesh";
 import { HTMLFile } from "tandem-html-extension/models/html-file";
-import { Observable } from "tandem-common/observable";
 import { MetadataKeys } from "tandem-front-end/constants";
 import { DocumentFile } from "tandem-front-end/models";
-import { watchProperty } from "tandem-common/observable";
 import { GroupNodeSection } from "tandem-html-extension/dom";
 import { CSSStylesheetsDependency } from "tandem-html-extension/dependencies";
 import { parseCSS, CSSRootExpression } from "tandem-html-extension/ast";
-import { Dependencies, DEPENDENCIES_NS, Injector } from "tandem-common/dependencies";
-import {BaseEntity, EntityMetadata, IEntityDocument } from "tandem-common/ast/entities";
+import { inject, Action, Observable, BaseEntity } from "tandem-common";
 
 import { CSSRuleEntity } from "./rule";
 import  { ICSSRuleEntity } from "./base";
@@ -18,6 +15,12 @@ export class CSSRootEntity extends BaseEntity<CSSRootExpression> {
   public owner: HTMLFile;
   public content: string;
   public document: DocumentFile<any>;
+  private _useASTAsContent: boolean;
+
+  constructor(source: CSSRootExpression) {
+    super(source);
+    source.observe(new WrapBus(this.onSourceAction.bind(this)));
+  }
 
   async mapContext(context) {
     this.content = await this.loadCSS(context);
@@ -27,13 +30,20 @@ export class CSSRootEntity extends BaseEntity<CSSRootExpression> {
   }
 
   async loadCSS(context) {
-    return this.document.content;
+
+    // slight optimization -- if the ast is modified, then we want to use that
+    // for the HTML document, otherwise we want to go the faster rought and use the document content
+    return this._useASTAsContent ? this.source.toString() : this.document.content;
   }
 
   getInitialMetadata() {
     return Object.assign(super.getInitialMetadata(), {
       [MetadataKeys.HIDDEN]: true
     });
+  }
+
+  protected onSourceAction(action: Action) {
+    this._useASTAsContent = true;
   }
 
   cloneLeaf() {
