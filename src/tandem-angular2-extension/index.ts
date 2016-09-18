c32c6f1a5cbb743ce41e385820187b6a664098d7
Craig Condon <craig.j.condon@gmail.com>
2016-09-18 22:53:34 +0900

    get ng compiler to work'

--- src/tandem-angular2-extension/index.ts
+++ src/tandem-angular2-extension/index.ts
@@ -1,17 +1,21 @@
 import { Component } from "@angular/core";
+import { __compiler_private__ as _ng, I18NHtmlParser, BoundTextAst, OfflineCompiler, CompileDirectiveMetadata, CompileTypeMetadata } from "@angular/compiler";
 
-@Component({
-  template: `<div>
-    hello World
-  </div>`
-})
-class SomeComponent {
 
-}
+const htmlParser = new I18NHtmlParser(new _ng.HtmlParser());
+const expParser = new _ng.Parser(new _ng.Lexer());
+const elementSchemaRegistry = new _ng.DomElementSchemaRegistry();
 
-const component = new SomeComponent();
-console.log(component);
 
+const tmplParser = new _ng.TemplateParser(expParser, elementSchemaRegistry, htmlParser, console, []);
+
+
+const metadata = new CompileDirectiveMetadata();
+metadata.viewProviders = [];
+metadata.type = new CompileTypeMetadata();
+const ast = tmplParser.parse(metadata, "hello {{name || last}}<span>test</span>", [], [], [], "");
+
+console.log(ast);
 
 export const angular2ExtensionDependency = [
 
