8e2a1a8362082a4af3d0d50ccd35cb073fd6df2a
Craig Condon <craig.j.condon@gmail.com>
2016-09-27 17:25:18 +0800

    fix hr tag

--- examples/typescript/hello-world.tsx
+++ examples/typescript/hello-world.tsx
@@ -4,7 +4,8 @@ const render = (a, b) => {
   return <div class="item" style="color:orange;font-size:60px;">hello {a} {b}</div>;
 }
 
+
 const element = document.createElement("div");
 // element.innerHTML = "<div style='color:white;'>hello</div>";
 document.body.appendChild(element);
-renderJSX(render("a", "bfsdfsd"), element);
+renderJSX(render("a", "fsfsfsd"), element);
