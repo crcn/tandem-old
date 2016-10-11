a1ba00bc11bffb59f3d06ee557c2feb76c73392d
Craig Condon <craig.j.condon@gmail.com>
2016-10-10 22:14:58 -0500

    add rAF

--- src/@tandem/synthetic-browser/renderers/dom.ts
+++ src/@tandem/synthetic-browser/renderers/dom.ts
@@ -25,28 +25,34 @@ export class SyntheticDOMRenderer extends BaseRenderer {
 
   update() {
     return new Promise((resolve) => {
-      ReactDOM.render(this.entity.render(), this.element, () => {
-        const syntheticComponentsBySourceUID = {};
 
-        for (const component of flattenTree(this.entity)) {
-          syntheticComponentsBySourceUID[component.uid] = component;
-        }
+      // rAF to prevent layout thrashing
+      requestAnimationFrame(() => {
 
-        const rects = {};
+        // render immediately to the DOM element
+        ReactDOM.render(this.entity.render(), this.element, () => {
+          const syntheticComponentsBySourceUID = {};
 
-        for (const node of this.element.querySelectorAll("*")) {
-          const element = <HTMLElement>node;
-          if (!element.dataset) continue;
-          const uid = element.dataset["uid"];
-          const sourceComponent: BaseSyntheticDOMNodeEntity<any, any> = syntheticComponentsBySourceUID[uid];
-          if (sourceComponent) {
-            sourceComponent.target = <HTMLElement>node;
+          for (const component of flattenTree(this.entity)) {
+            syntheticComponentsBySourceUID[component.uid] = component;
           }
-          rects[uid] = BoundingRect.fromClientRect(element.getBoundingClientRect());
-        }
 
-        this.setRects(rects);
-        resolve();
+          const rects = {};
+
+          for (const node of this.element.querySelectorAll("*")) {
+            const element = <HTMLElement>node;
+            if (!element.dataset) continue;
+            const uid = element.dataset["uid"];
+            const sourceComponent: BaseSyntheticDOMNodeEntity<any, any> = syntheticComponentsBySourceUID[uid];
+            if (sourceComponent) {
+              sourceComponent.target = <HTMLElement>node;
+            }
+            rects[uid] = BoundingRect.fromClientRect(element.getBoundingClientRect());
+          }
+
+          this.setRects(rects);
+          resolve();
+        });
       });
     });
   }
