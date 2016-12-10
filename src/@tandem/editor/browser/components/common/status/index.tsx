45eeead9e1fcf635128f50fa2f518582f89b4b6f
Craig Condon <craig.j.condon@gmail.com>
2016-12-09 21:33:23 -0600

    fix janky loader

--- src/@tandem/editor/browser/components/common/status/index.tsx
+++ src/@tandem/editor/browser/components/common/status/index.tsx
@@ -5,24 +5,24 @@ import { Status } from "@tandem/common/status";
 export class StatusComponent extends React.Component<{ status: Status, className?: string, style?: any }, any> {
   render() {
     const { status, className, style } = this.props;
-    if (!status) return null;
-
     let section;
 
-    if (status.type === Status.LOADING) {
-      section = <span className="spinner" />
-    } else if (status.type === Status.ERROR) {
-      section = <span className="error-icon" title={status.data.stack}>
-        <i className="ion-alert-circled" />
-      </span>
-    } else if (status.type === Status.COMPLETED) {
-      // section = <span className="completed-icon">
-      //   <i className="ion-checkmark-round" />
-      // </span>
+    if (status) {
+      if (status.type === Status.LOADING) {
+        section = <span className="spinner" />
+      } else if (status.type === Status.ERROR) {
+        section = <span className="error-icon" title={status.data.stack}>
+          <i className="ion-alert-circled" />
+        </span>
+      } else if (status.type === Status.COMPLETED) {
+        section = <span className="completed-icon">
+          &nbsp;
+        </span>
+      }
     }
 
     return <span className={["status-component", className].join(" ")} style={style}>
-      { section }
+      { section || <span>&nbsp;</span> }
     </span>;
   }
 }
