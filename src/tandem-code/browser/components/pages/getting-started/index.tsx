91a385fe78fe46fdfe9cbc2355af7ef85aa4b136
Craig Condon <craig.j.condon@gmail.com>
2017-01-20 15:32:07 +0800

    get started on getting started page

--- src/tandem-code/browser/components/pages/getting-started/index.tsx
+++ src/tandem-code/browser/components/pages/getting-started/index.tsx
@@ -1,6 +1,7 @@
 import "./index.scss";
 
-import * as React from "react";
+import React = require("react");
+import cx = require("classnames");
 
 export class GettingStartedComponent extends React.Component<any, any> {
 
@@ -15,7 +16,8 @@ export class GettingStartedComponent extends React.Component<any, any> {
     const pages = [
       this.renderHello,
       this.renderInstallExtensions,
-      this.howToUse
+      // this.howToUse,
+      this.done
     ];
 
     return <div className="getting-started-component">
@@ -26,7 +28,7 @@ export class GettingStartedComponent extends React.Component<any, any> {
         <div className="row">
           <div className="col-12">
             <a href="#" className="button pull-right" onClick={this.nextPage.bind(this)}>
-              Next
+              { this._page === pages.length -1 ? "Start using tandem" : "Next" }
             </a>
           </div>
         </div>
@@ -37,28 +39,86 @@ export class GettingStartedComponent extends React.Component<any, any> {
   renderHello = () => {
     return <div>
       <div className="hello row">
-        <div className="logo">
-          <img src={require("./icon_black.png")} />
-          Tandem
-        </div>
+        <div className="col-12">
+          <div className="logo">
+            <img src={require("./icon_black.png")} />
+            Tandem
 
-        <div className="description">
-          Welcome to Tandem. This brief guide will help set you up so that you get the most of the
-          the application.
+          </div>
+          <div className="description">
+            Welcome to Tandem! This brief guide will help you get started.
+          </div>
         </div>
       </div>
     </div>
   }
 
+  // TODO - auto-detect extension here
   renderInstallExtensions = () => {
-    return <div>
-      Install extensions
-    </div>
+
+    // TODO - should not hard-code here. Okay for MVP.
+    const options = [
+      {
+        label: "Atom",
+        iconUrl: require("./atom.png"),
+        selected: true
+      },
+      {
+        label: "VSCode",
+        iconUrl: require("./vscode.png"),
+        selected: false
+      },
+      {
+        label: "None",
+        iconUrl: require("./icon_blue.png"),
+        selected: false
+      }
+    ];
+
+    return <div className="install-text-editor-extension">
+      <div className="row">
+        <div className="col-12">
+          <div className="title">
+            Install a text editor extension
+          </div>
+          <div className="description">
+            Tandem works best with your text editor to synchronize code changes as you're building your application. 
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <ul className="options">
+            {
+              options.map(({ label, iconUrl, selected }) => {
+                return <li className={cx({ selected: selected }, "fill-text")}>
+                <img src={iconUrl} />
+                <label>{label}</label>
+              </li>
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    </div>;
   }
 
   howToUse = () => {
     return <div>
       How to use
     </div>
+  }
+
+  done = () => {
+    return <div className="done">
+      <div className="row">
+        <div className="col-12">
+          <div className="title">
+            All done! You can now start using Tandem.
+            
+          </div>
+        </div>
+      </div>
+    </div>;
   }
 }
