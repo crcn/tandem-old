839441664603f16ab3b970907519cf30160264b0
Craig Condon <craig.j.condon@gmail.com>
2017-01-26 10:57:28 -0500

    update console style

--- src/@tandem/html-extension/editor/browser/components/panes/bottom-gutter/animations/index.tsx
+++ src/@tandem/html-extension/editor/browser/components/panes/bottom-gutter/animations/index.tsx
@@ -3,7 +3,7 @@ import React = require("react");
 export class AnimationsComponent extends React.Component<any, any> {
   render() {
     return <div className="animations">
-      Animations
+      Animations.
     </div>;
   }
 }
--- src/@tandem/html-extension/editor/browser/components/panes/bottom-gutter/console/index.tsx
+++ src/@tandem/html-extension/editor/browser/components/panes/bottom-gutter/console/index.tsx
@@ -1,9 +1,15 @@
+import "./index.scss";
 import React = require("react");
 
 export class ConsoleComponent extends React.Component<any, any> {
   render() {
     return <div className="console-gutter-component">
-      console
+      <ul className="logs">
+        <li className="error">Error</li>
+        <li className="warning">Warning</li>
+        <li className="notice">Notice me this</li>
+        <li>Log one</li>
+      </ul>
     </div>;
   }
 }
