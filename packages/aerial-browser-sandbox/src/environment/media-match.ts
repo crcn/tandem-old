ecf3ac942e16ad1f430f0808fa1cdf9a19018322
Craig Condon <craig.j.condon@gmail.com>
2017-10-06 14:45:08 -0400

    more cases for media text

--- packages/aerial-browser-sandbox/src/environment/media-match.ts
+++ packages/aerial-browser-sandbox/src/environment/media-match.ts
@@ -9,7 +9,7 @@ const getMediaTextTokens = (mediaText: string) => {
     let c = mediaText.charAt(cursor);
     const rest = mediaText.substr(cursor);
 
-    const match = rest.match(/^([a-zA-Z0-9-]+|\(|\)|\:|\s|\t)/);
+    const match = rest.match(/^([a-zA-Z0-9-]+|\(|\)|\:|\s|\t|\,|\/|\+|\-)/);
 
     if (!match) {
       throw new Error(`Syntax error, unexpected token ${c} in ${mediaText}.`);
@@ -29,37 +29,52 @@ const getMediaTextTokens = (mediaText: string) => {
 // only support pixels for now
 const calcMeasurement = (value: string, window: SEnvWindowInterface) => value.replace("px", "");
 
-const translateMediaText = (mediaText: string) => {
-  const tokens = getMediaTextTokens(mediaText);
-  let cursor: number = 0;
-  let buffer = [];
-
+const getMediaJSExpression = (cursor, tokens: string[], until?: string) => {
+  const buffer = [];
   while (cursor < tokens.length) {
     const token = tokens[cursor];
+    if (token === until) {
+      break;
+    }
     cursor++;
 
-    if (token === "(") {
-
+    if (token === "(" || token === ")") {
     } else if (token === "screen") {
 
       // for now -- later we can do context.type === "screen"
       buffer.push("true");
     } else if (token === "min-width") {
-      buffer.push("context.innerWidth", ">", `calcMeasurement("${tokens[++cursor]}"`,", context)");
+      const chunk = getMediaJSExpression(cursor, tokens, ")");
+      cursor += chunk.length;
+      console.log(tokens.slice(cursor));
+      buffer.push("context.innerWidth", ">", `calcMeasurement("${chunk}"`,", context)");
     } else if (token === "max-width") {
-      buffer.push("context.innerWidth", "<", `calcMeasurement("${tokens[++cursor]}"`,", context)");
+      const chunk = getMediaJSExpression(cursor, tokens, ")");
+      cursor += chunk.length;
+      buffer.push("context.innerWidth", "<", `calcMeasurement("${chunk}"`,", context)");
     } else if (token === "min-height") {
-      buffer.push("context.innerHeight", ">", `calcMeasurement("${tokens[++cursor]}"`,", context)");
+      const chunk = getMediaJSExpression(cursor, tokens, ")");
+      cursor += chunk.length;
+      buffer.push("context.innerHeight", ">", `calcMeasurement("${chunk}"`,", context)");
     } else if (token === "max-height") {
-      buffer.push("context.innerHeight", "<", `calcMeasurement("${tokens[++cursor]}"`,", context)");
+      const chunk = getMediaJSExpression(cursor, tokens, ")");
+      cursor += chunk.length;
+      buffer.push("context.innerHeight", "<", `calcMeasurement("${chunk}"`,", context)");
     } else if (token === "and") {
       buffer.push("&&");
-    } else if (token === "or") {
+    } else if (token === "or" || token === ",") {
       buffer.push("&&");
+    } else {
+      buffer.push(token);
     }
   }
 
   return buffer.join(" ");
+}
+
+const translateMediaText = (mediaText: string) => {
+  console.log(mediaText, getMediaJSExpression(0, getMediaTextTokens(mediaText)));
+  return getMediaJSExpression(0, getMediaTextTokens(mediaText));
 };
 
 const compileMediaText = weakMemo((mediaText: string) => new Function("context", "calcMeasurement", `return ${translateMediaText(mediaText)}`));
