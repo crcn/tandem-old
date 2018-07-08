8418ea0d3b7ba0ffa93ccff7a83889c09e3435d3
Craig Condon <craig.j.condon@gmail.com>
2018-07-08 14:50:57 -0300

    move borders below

--- packages/front-end/src/components/root/workspace/right-gutter/styles/pretty/panes/borders-controller.tsx
+++ packages/front-end/src/components/root/workspace/right-gutter/styles/pretty/panes/borders-controller.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { compose, pure, withHandlers } from "recompose";
 import { cssPropertyChangeCompleted, cssPropertyChanged } from "actions";
+import { memoize } from "tandem-common";
 
 export default compose(
   pure,
   withHandlers({
     onPropertyChange: ({ dispatch }) => (name, value) => {
+      console.log(name, value);
       dispatch(cssPropertyChanged(name, value));
     },
     onPropertyChangeComplete: ({ dispatch }) => (name, value) => {
@@ -23,7 +25,51 @@ export default compose(
           onPropertyChange,
           onPropertyChangeComplete
         }}
+        bottomLeftRadiusInputProps={{
+          onChange: propertyChangeCallback(
+            "border-bottom-left-radius",
+            onPropertyChange
+          ),
+          onChangeComplete: propertyChangeCallback(
+            "border-bottom-left-radius",
+            onPropertyChangeComplete
+          )
+        }}
+        bottomRightRadiusInputProps={{
+          onChange: propertyChangeCallback(
+            "border-bottom-right-radius",
+            onPropertyChange
+          ),
+          onChangeComplete: propertyChangeCallback(
+            "border-bottom-right-radius",
+            onPropertyChangeComplete
+          )
+        }}
+        topLeftRadiusInputProps={{
+          onChange: propertyChangeCallback(
+            "border-top-left-radius",
+            onPropertyChange
+          ),
+          onChangeComplete: propertyChangeCallback(
+            "border-top-left-radius",
+            onPropertyChangeComplete
+          )
+        }}
+        topRightRadiusInputProps={{
+          onChange: propertyChangeCallback(
+            "border-top-right-radius",
+            onPropertyChange
+          ),
+          onChangeComplete: propertyChangeCallback(
+            "border-top-right-radius",
+            onPropertyChangeComplete
+          )
+        }}
       />
     );
   }
+);
+
+const propertyChangeCallback = memoize((name: string, listener) => value =>
+  listener(name, value)
 );
