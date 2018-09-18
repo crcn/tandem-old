590f9483efd1a1f67e06fcac2feb72222f6c53ae
Craig Condon <craig.j.condon@gmail.com>
2018-09-18 11:21:27 -0500

    fix build issues

--- packages/front-end/src/components/root/workspace/right-gutter/variables/row-item-controller.tsx
+++ packages/front-end/src/components/root/workspace/right-gutter/variables/row-item-controller.tsx
@@ -4,7 +4,10 @@ import { Dispatch } from "redux";
 import { PCVariable, PCVariableType } from "paperclip";
 import { BaseVariableRowItemProps } from "./view.pc";
 import { DropdownMenuOption } from "../../../../inputs/dropdown/controller";
-import {variableLabelChanged, variableValueChanged} from "../../../../../actions";
+import {
+  variableLabelChanged,
+  variableValueChanged
+} from "../../../../../actions";
 import { EMPTY_ARRAY } from "tandem-common";
 import { FontFamily } from "../../../../../state";
 import { getFontFamilyOptions } from "../styles/pretty/panes/typography-controller";
@@ -15,40 +18,57 @@ export type Props = {
   fontFamilies: FontFamily[];
 };
 
+export default (Base: React.ComponentClass<BaseVariableRowItemProps>) =>
+  class VariableRowItemController extends React.PureComponent<Props> {
+    onValueChange = value => {
+      this.props.dispatch(variableValueChanged(this.props.variable, value));
+    };
+    onLabelChange = value => {
+      this.props.dispatch(variableLabelChanged(this.props.variable, value));
+    };
+    render() {
+      const { onValueChange, onLabelChange } = this;
+      const { variable, fontFamilies, ...rest } = this.props;
+      const limited = variable.type === PCVariableType.FONT;
+      const color = variable.type === PCVariableType.COLOR;
+      const unlimited =
+        variable.type === PCVariableType.NUMBER ||
+        variable.type === PCVariableType.UNIT;
 
+      let limitedOptions: DropdownMenuOption[] = EMPTY_ARRAY;
 
-export default (Base: React.ComponentClass<BaseVariableRowItemProps>)  => class VariableRowItemController extends React.PureComponent<Props> {
-  onValueChange = (value) => {
-    this.props.dispatch(variableValueChanged(this.props.variable, value));
-  }
-  onLabelChange = (value) => {
-    this.props.dispatch(variableLabelChanged(this.props.variable, value));
-  }
-  render() {
-    const {onValueChange, onLabelChange, onTypeChange} = this;
-    const {variable, fontFamilies, ...rest} = this.props;
-    const limited = variable.type === PCVariableType.FONT;
-    const color = variable.type === PCVariableType.COLOR;
-    const unlimited = variable.type === PCVariableType.NUMBER || variable.type === PCVariableType.UNIT;
+      if (variable.type === PCVariableType.FONT) {
+        limitedOptions = getFontFamilyOptions(fontFamilies);
+      }
 
-    let limitedOptions: DropdownMenuOption[] = EMPTY_ARRAY;
-
-    if (variable.type === PCVariableType.FONT) {
-      limitedOptions = getFontFamilyOptions(fontFamilies);
+      return (
+        <Base
+          {...rest}
+          variant={cx({
+            unlimited,
+            limited,
+            color
+          })}
+          limitedInputProps={{
+            options: limitedOptions,
+            onChange: onValueChange,
+            value: variable.value
+          }}
+          unlimitedInputProps={{
+            onChange: onValueChange,
+            value: variable.value
+          }}
+          nameInputProps={{
+            focus: !variable.label,
+            onChangeComplete: onLabelChange,
+            value: variable.label
+          }}
+          colorInputProps={{
+            onChange: onValueChange,
+            onChangeComplete: onValueChange,
+            value: variable.value
+          }}
+        />
+      );
     }
-
-    return <Base {...rest} variant={cx({
-      unlimited,
-      limited,
-      color,
-    })} limitedInputProps={{
-      options: limitedOptions,
-      onChange: onValueChange,
-      value: variable.value
-    }} unlimitedInputProps={{onChange: onValueChange, value: variable.value}} nameInputProps={{focus: !variable.label, onChangeComplete: onLabelChange, value: variable.label}} colorInputProps={{
-      onChange: onValueChange,
-      onChangeComplete: onValueChange,
-      value: variable.value
-    }} />;
-  }
-}
+  };
--- packages/front-end/src/components/root/workspace/right-gutter/variables/tab-controller.tsx
+++ packages/front-end/src/components/root/workspace/right-gutter/variables/tab-controller.tsx
@@ -3,24 +3,37 @@ import * as cx from "classnames";
 import { BaseVariablesTabProps } from "./view.pc";
 import { Dispatch } from "redux";
 import { PCVariable } from "paperclip";
-import { FontFamily } from "state";
+import { FontFamily } from "../../../../../state";
 
 export type Props = {
   dispatch: Dispatch<any>;
   globalFileUri: string;
   globalVariables: PCVariable[];
   fontFamilies: FontFamily[];
-}
+};
 
-export default (Base: React.ComponentClass<BaseVariablesTabProps>) => class VariablesTabController extends React.PureComponent<Props> {
-  render() {
-    const {dispatch, globalFileUri, globalVariables, fontFamilies, ...rest} = this.props;
-    return <Base {...rest} variant={cx({
-      noGlobalFile: !globalFileUri
-    })} variablesInputProps={{
-      dispatch,
-      fontFamilies,
-      globalVariables
-    }} />;
-  }
-}
+export default (Base: React.ComponentClass<BaseVariablesTabProps>) =>
+  class VariablesTabController extends React.PureComponent<Props> {
+    render() {
+      const {
+        dispatch,
+        globalFileUri,
+        globalVariables,
+        fontFamilies,
+        ...rest
+      } = this.props;
+      return (
+        <Base
+          {...rest}
+          variant={cx({
+            noGlobalFile: !globalFileUri
+          })}
+          variablesInputProps={{
+            dispatch,
+            fontFamilies,
+            globalVariables
+          }}
+        />
+      );
+    }
+  };
