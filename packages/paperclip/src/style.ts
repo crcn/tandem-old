032540b6bef466321e280e900bab9c0bcff474a2
Craig Condon <craig.j.condon@gmail.com>
2018-10-03 11:32:26 -0500

    fix #534

--- packages/paperclip/src/style.ts
+++ packages/paperclip/src/style.ts
@@ -1,6 +1,10 @@
 import { memoize, KeyValue } from "tandem-common";
 import { defaults } from "lodash";
-import { InspectorNode, getInspectorNodeOverrides } from "./inspector";
+import {
+  InspectorNode,
+  getInspectorNodeOverrides,
+  getInspectorNodeByAssocId
+} from "./inspector";
 import {
   PCComponent,
   PCVisibleNode,
@@ -16,12 +20,25 @@ import {
 } from "./dsl";
 import { DependencyGraph } from "./graph";
 
+export type ComputeStyleOptions = {
+  inheritedStyles?: boolean;
+  overrides?: boolean;
+  parentStyles?: boolean;
+};
+
+const DEFAULT_COMPUTE_STYLE_OPTIONS: ComputeStyleOptions = {
+  inheritedStyles: true,
+  overrides: true,
+  parentStyles: true
+};
+
 export const computeStyleInfo = memoize(
   (
     inspectorNodes: InspectorNode[],
     rootInspectorNode: InspectorNode,
     variant: PCVariant,
-    graph: DependencyGraph
+    graph: DependencyGraph,
+    options: ComputeStyleOptions = DEFAULT_COMPUTE_STYLE_OPTIONS
   ): ComputedStyleInfo => {
     const style = {};
     const sourceNodes: (PCVisibleNode | PCComponent)[] = [];
@@ -33,34 +50,62 @@ export const computeStyleInfo = memoize(
         | PCComponent;
       sourceNodes.push(sourceNode);
       let current: PCNode = sourceNode;
-      while (extendsComponent(current)) {
-        const parent: PCElement = getPCNode(
-          (current as PCComponent).is,
-          graph
-        ) as PCElement;
-        if (isPCComponentInstance(parent)) {
-          // defaults -- parents cannot disable
-          defaults(style, parent.style);
+
+      if (options.parentStyles !== false) {
+        while (extendsComponent(current)) {
+          const parent: PCElement = getPCNode(
+            (current as PCComponent).is,
+            graph
+          ) as PCElement;
+          if (isPCComponentInstance(parent)) {
+            // defaults -- parents cannot disable
+            defaults(style, parent.style);
+          }
+          current = parent;
         }
-        current = parent;
       }
 
-      const overrides = getInspectorNodeOverrides(
-        inspectorNode,
-        rootInspectorNode,
-        variant,
-        graph
-      );
-
       Object.assign(style, sourceNode.style);
-      for (const override of overrides) {
-        if (override.propertyName === PCOverridablePropertyName.STYLE) {
-          for (const key in override.value) {
-            if (!styleOverridesMap[key]) {
-              styleOverridesMap[key] = [];
+
+      if (options.inheritedStyles !== false && sourceNode.inheritStyle) {
+        for (const styleMixinId in sourceNode.inheritStyle) {
+          const styleMixinInspectorNode = getInspectorNodeByAssocId(
+            styleMixinId,
+            rootInspectorNode
+          );
+          if (!styleMixinInspectorNode) {
+            continue;
+          }
+
+          defaults(
+            style,
+            computeStyleInfo(
+              toArray(styleMixinInspectorNode),
+              rootInspectorNode,
+              null,
+              graph
+            ).style
+          );
+        }
+      }
+
+      if (options.overrides !== false) {
+        const overrides = getInspectorNodeOverrides(
+          inspectorNode,
+          rootInspectorNode,
+          variant,
+          graph
+        );
+
+        for (const override of overrides) {
+          if (override.propertyName === PCOverridablePropertyName.STYLE) {
+            for (const key in override.value) {
+              if (!styleOverridesMap[key]) {
+                styleOverridesMap[key] = [];
+              }
+              styleOverridesMap[key].push(override);
+              style[key] = override.value[key];
             }
-            styleOverridesMap[key].push(override);
-            style[key] = override.value[key];
           }
         }
       }
@@ -73,3 +118,5 @@ export const computeStyleInfo = memoize(
     };
   }
 );
+
+const toArray = memoize(<TValue>(value: TValue) => [value]);
