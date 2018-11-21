91066f25f36984d6364b6fba0dabb1a477dad08a
Craig Condon <craig.j.condon@gmail.com>
2018-11-21 13:40:52 +0200

    make icon bigger

--- packages/paperclip-migrator/005-006.js
+++ packages/paperclip-migrator/005-006.js
@@ -25,7 +25,25 @@ module.exports = (module) => {
       }
     }
 
-    return node;
+    if (node.name === "variant-trigger" && node.source && node.source.type === 0) {
+      return {
+        id: node.id,
+        children: node.children,
+        metadata: node.metadata,
+        label: node.label,
+        name: "variant-trigger",
+        targetVariantId: node.targetVariantId,
+        source: {
+          type: 0,
+          queryId: node.source.mediaQueryId
+        }
+      }
+    }
+
+    return {
+      ...node,
+      children: node.children.map(mapNode)
+    };
   };
 
   return mapModule(module);
