e7e1e1e3e5719670bdc422ff2a7848161d678769
Craig Condon <craig.j.condon@gmail.com>
2018-11-28 07:45:57 +0200

    add auto updater

--- packages/desktop/src/desktop/index.ts
+++ packages/desktop/src/desktop/index.ts
@@ -1,11 +1,12 @@
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware } from "redux";
-
 import { DesktopState } from "./state";
-
 import { rootReducer } from "./reducers";
-
 import { rootSaga } from "./sagas";
+
+try {
+  require("update-electron-app")();
+} catch (e) {}
 
 export const init = (initialState: DesktopState) => {
   const sagaMiddleware = createSagaMiddleware();
