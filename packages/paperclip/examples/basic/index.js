a4346faa7eac623a3df0eb95ba722b26fa6e7c66
Craig Condon <craig.j.condon@gmail.com>
2020-01-30 13:54:11 -0800

    get net version to work

--- packages/paperclip/examples/basic/index.js
+++ packages/paperclip/examples/basic/index.js
@@ -1,15 +1,28 @@
-const { Engine } = require("../..");
-const fs = require("fs");
-const e = new Engine({
-  httpPath: "http://oka"
-});
-e.onEvent(event => {
-  console.log(event);
+const jayson = require("jayson");
+
+// create a client
+const client = jayson.client.tcp({
+  path: "/tmp/json-ipc-test.ipc"
 });
 
-let now = Date.now();
-// for (let i = 100; i--;)
-e.startRuntime(__dirname + "/main.pc");
-console.log(Date.now() - now);
+for (let i = 1000; i--; )
+  client.request("say_hello", [], function(err, response) {
+    if (err) throw err;
+    console.log(response.result); // 2
+  });
 
-// console.log(JSON.stringify(e.drainEvents(), null, 2));
+// const { Engine } = require("../..");
+// const fs = require("fs");
+// const e = new Engine({
+//   httpPath: "http://oka"
+// });
+// e.onEvent(event => {
+//   console.log(event);
+// });
+
+// let now = Date.now();
+// // for (let i = 100; i--;)
+// e.startRuntime(__dirname + "/main.pc");
+// console.log(Date.now() - now);
+
+// // console.log(JSON.stringify(e.drainEvents(), null, 2));
--- packages/paperclip/native/src/main.rs
+++ packages/paperclip/native/src/main.rs
@@ -8,55 +8,24 @@ mod css;
 mod pc;
 mod js;
 
+use std::sync::Arc;
+use std::{thread, time};
+
 use pc::runtime::graph::{DependencyGraph};
 use js::runtime::virt::{JsObject, JsValue};
+use jsonrpc_core::*;
+use jsonrpc_ipc_server::ServerBuilder;
+use jsonrpc_ipc_server::jsonrpc_core::*;
+
+use jsonrpc_core::futures::Future;
 
 fn main() {
-
-    /*
-    let mut io = IoHandler::new();
-	io.add_method("say_hello", |_params: Params| {
-		Ok(Value::String("hello".to_string()))
+	let mut io = IoHandler::new();
+	io.add_method("say_hello", |_params| {
+		Ok(Value::String("hello world".into()))
 	});
 
-	let server = ServerBuilder::new(io)
-		.threads(3)
-		.start_http(&"127.0.0.1:3030".parse().unwrap())
-		.unwrap();
-
-    server.wait();
-    */
-
-    // // let mut f = File::open("test.pc");
-
-    // let buffer = "<style></style> <div></div>".to_string();
-
-    // // let mut buffer = String::new();
-    // // // read the whole file
-    // // f.unwrap().read_to_string(&mut buffer);
-
-    // println!("---------------------------");
-    // println!("Input: ");
-    // println!("---------------------------");
-    // println!("{}", buffer);
-    // println!("---------------------------");
-
-    // println!("OKOK");
-
-    // // // let now = Instant::now();
-    // let expr = pc::parser::parse(buffer.as_str()).unwrap();
-    // println!("{:?}", &expr);
-    // // // println!("micro seconds to parse: {}", now.elapsed().as_micros());
-
-
-    // // // let now = Instant::now();
-    // let result = pc::runtime::evaluate(&expr, &"something".to_string(), &DependencyGraph::new(), &JsValue::JsObject(JsObject::new())).unwrap().unwrap();
-
-    // println!("{:?}", result);
-
-    // // // println!("micro seconds to evaluate: {}", now.elapsed().as_micros());
-    // // // println!("{}", result);
-    // // let serialized = serde_json::to_string(&result).unwrap();
-
-    // // println!("json: {}", serialized);
+	let builder = ServerBuilder::new(io);
+	let server = builder.start("/tmp/json-ipc-test.ipc").expect("Couldn't open socket");
+	server.wait();
 }
