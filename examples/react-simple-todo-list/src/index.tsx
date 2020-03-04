df6d5cfa997d9a50706affe39483466301f140ad
Craig Condon <craig@captec.io>
2020-03-04 09:19:18 -0600

    fix casing

--- examples/react-simple-todo-list/src/index.tsx
+++ examples/react-simple-todo-list/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import List from "./List";
+import List from "./list";
 
 const mount = document.createElement("div");
 ReactDOM.render(<List />, mount);
